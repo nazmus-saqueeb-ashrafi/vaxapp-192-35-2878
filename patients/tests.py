76ba60623ff960d8db3b3de96a3019c3e16fb409
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2021-12-25 04:11:39 +0600

    Commit 11- added tests

--- patients/tests.py
+++ patients/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+from .forms import PatientModelForm
+
+
+class LandingPageTest(TestCase):
+
+    def test_get(self):
+        response = self.client.get(reverse("landing-page"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing.html")
+
+
+class TestPatientCreateForm(TestCase):
+    def test_login_form_valid_data(self):
+        form = PatientModelForm(data={
+            'patient_first_name': 'Nazmus',
+            'patient_last_name': 'Ashrafi',
+            'patient_email': 'nazmus.as@gmail.com',
+            'patient_username': 'nazmus',
+
+            'patient_age': '10',
+            'patient_gender': 'Male',
+            'patient_maternal_status': 'Not Pregnant',
+            'patient_smoking_status': 'Smoker',
+            'patient_job_status': 'Unemployed',
+            'patient_NID': '2222',
+            'patient_vaccinator': '',
+            'organisation': 'nazmus'
+
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_login_form_no_data(self):
+        form = PatientModelForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 10)
